a8c941e19ef0ee2bb756e53567ea6459bd523301
Zett Stai <lizettestai@gmail.com>
2017-06-04 23:07:41 -0700

    Creating backend script

---
+++ backend.py
@@ -0,0 +1,5 @@
+# Choose a task to be accomplished.
+# Set the Pomodoro to 25 minutes (the Pomodoro is the timer)
+# Work on the task until the Pomodoro rings, then put a check on your sheet of paper
+# Take a short break (5 minutes is OK)
+# Every 4 Pomodoros take a longer break
